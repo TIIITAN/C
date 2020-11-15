28a776b3b83bfe9f739fd331308363eed7dbd6dc
TIIITAN <74502713+TIIITAN@users.noreply.github.com>
2020-11-15 17:26:51 +0100

    Create pointers.h

---
+++ enibook/exercices/401/pointers.h
@@ -0,0 +1,10 @@
+#ifndef POINTERS
+#define POINTERS
+
+void adjustMin(double* a, double* b);
+
+void adjustMax(double* c, double* d);
+
+void decomposeTime(double duration, int *ptr_heure, double *ptr_minute);
+
+#endif
